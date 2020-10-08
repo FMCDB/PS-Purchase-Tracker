73e47d079dd102682a3f5613602b5ffa77b68c01
Finian Bradwell <finianbradwell708@gmail.com>
2020-10-08 14:33:29 +0100

    Database is no longer emptied when program stops running, Data is displayed in correct order on chart

--- purchase_tracker.py
+++ purchase_tracker.py
@@ -15,23 +15,25 @@ import unicodedata
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-connection = sqlite3.connect('database.db', check_same_thread=False)
-
-with open('schema.sql') as f:
-    connection.executescript(f.read())
-
 def get_db_connection():
-	conn = sqlite3.connect('database.db')
-	conn.row_factory = sqlite3.Row
-	return conn
+	con = sqlite3.connect('database.db')
+	con.row_factory = sqlite3.Row
+	return con
 
 app = Flask(__name__)
-
+    
 @app.route('/')
 def index():
-    return render_template('base.html', purchases=start())
+    update()
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute("select * from purchases order by id desc")
+    purchases = cur.fetchall()
+    return render_template("base.html",purchases = purchases)
 
-def start():
+
+
+def update():
     
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -56,16 +58,11 @@ def start():
     user_id = "me"
     query = 'from:Playstation subject:(\'Thank You For Your Purchase\')'
     
-    conn = get_db_connection()
-    purchases = conn.execute('SELECT id FROM purchases').fetchall()
-    purchase_ids = [ sub['id'] for sub in purchases ] 
-    
+    con = get_db_connection()
+    purchases = con.execute('SELECT id FROM purchases').fetchall()
+    purchase_ids = [ sub['id'] for sub in purchases ]     
     check_for_new_purchases(service, user_id, query, purchase_ids)
-    connection.commit()
     
-    new_purchases = conn.execute('SELECT * FROM purchases').fetchall()
-    
-    return new_purchases
     
 
 def GetMessage(service, user_id, msg_id):
@@ -130,95 +127,98 @@ def update_messages(response, messages, purchase_ids):
     return response, messages
 
 
-
 def update_purchases(service, user_id, messages):
     
     # Extracts relevant data from emails and inserts this into database
     
-    cur = connection.cursor()
+    with sqlite3.connect("database.db") as con:
+        
+        cur = con.cursor()
     
-    for msg_id in messages:
-        
-        html_doc = GetMessage(service, user_id, msg_id)
-        
-        #HTML PARSING:
-        # Title: 1st use of <strong>
-        # Price: First use of euro sign ( € )
-        # Date: date in email
-        
-        invalid = ['Fund Sources Used (Total)', 
-                   'Next payment date:', 
-                   'To turn off automatic funding from:',
-                   'At the time of making your purchase, you asked us to provide you with immediate access to your purchase and confirmed your understanding that this means:']
-        
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        strong_tags = soup.find_all('strong')
-        b_tags = soup.find_all('b')
-        a_tags = soup.find_all('a')
-        td_tags_bottom = soup.findAll('td', {'valign' : 'bottom'}, {'align' : 'left'})
-        td_tags_middle = soup.findAll('td', {'valign' : 'middle'}, {'align' : 'left'})
-        
-        if len(strong_tags) > 0 and (strong_tags[0].get_text().strip() not in invalid):    
-             title_container = strong_tags[0]
-        elif len(b_tags) > 0 and (b_tags[0].get_text().strip() not in invalid):
-            title_container = b_tags[0]
-        elif len(td_tags_bottom) > 0:
-            title_container = td_tags_bottom[0]
-        else:
-            title_container = a_tags[2]
+        for msg_id in messages:
             
-        #Title
-        title = title_container.get_text().strip()
-        if 'Current Wallet Amount' in title:
-            title_container = td_tags_middle[4]
-            if len(title_container.get_text().strip()) == 0:   
-                title_container = td_tags_middle[6]
+            html_doc = GetMessage(service, user_id, msg_id)
+            
+            #HTML PARSING:
+            # Title: 1st use of <strong>
+            # Price: First use of euro sign ( € )
+            # Date: date in email
+            
+            invalid = ['Fund Sources Used (Total)', 
+                       'Next payment date:', 
+                       'To turn off automatic funding from:',
+                       'At the time of making your purchase, you asked us to provide you with immediate access to your purchase and confirmed your understanding that this means:']
+            
+            soup = BeautifulSoup(html_doc, 'html.parser')
+            strong_tags = soup.find_all('strong')
+            b_tags = soup.find_all('b')
+            a_tags = soup.find_all('a')
+            td_tags_bottom = soup.findAll('td', {'valign' : 'bottom'}, {'align' : 'left'})
+            td_tags_middle = soup.findAll('td', {'valign' : 'middle'}, {'align' : 'left'})
+            
+            if len(strong_tags) > 0 and (strong_tags[0].get_text().strip() not in invalid):    
+                 title_container = strong_tags[0]
+            elif len(b_tags) > 0 and (b_tags[0].get_text().strip() not in invalid):
+                title_container = b_tags[0]
+            elif len(td_tags_bottom) > 0:
+                title_container = td_tags_bottom[0]
+            else:
+                title_container = a_tags[2]
+                
+            #Title
             title = title_container.get_text().strip()
-        if '\r' in title:
-            title = title[:title.index('\r')-2]
+            if 'Current Wallet Amount' in title:
+                title_container = td_tags_middle[4]
+                if len(title_container.get_text().strip()) == 0:   
+                    title_container = td_tags_middle[6]
+                title = title_container.get_text().strip()
+            if '\r' in title:
+                title = title[:title.index('\r')-2]
+                
+            #Type
+            if title[-1] == ')':
+                start = title.rfind('(')+1
+                end = title.rfind(')')
+                purchase_type = title[start:end]
+                title = title[:title.find(purchase_type)-2]
             
-        #Type
-        if title[-1] == ')':
-            start = title.rfind('(')+1
-            end = title.rfind(')')
-            purchase_type = title[start:end]
-            title = title[:title.find(purchase_type)-2]
-        
-        text = soup.get_text()
-        
-        #Price
-        start = text.find('{}'.format(unicodedata.lookup("EURO SIGN")))
-        end = start + 6
-        price = text[start:end].strip()
-        
-        #Date
-        if ' AM' in text or ' PM' in text:
-            period = ' AM' if ' AM' in text else ' PM'
-            start = text.find(period) - 16
-            end = text.find(period) + 3
-        else:
-            heading = 'Date and time of purchase: '
-            start = text.find(heading) + len(heading)
-            end = start + 10
-        date = text[start:end].strip()
-        
-        if '@' in date:
-            date = text[start-2:end].strip()
-            date = date.replace('@','').replace(' ','',1)
+            text = soup.get_text()
             
-        purchase_dict = {
-            'Title': title,
-            'Type': purchase_type,
-            'Price': price,
-            'Date': date
-        }
-        
-        print(purchase_dict)
+            #Price
+            start = text.find('{}'.format(unicodedata.lookup("EURO SIGN")))
+            end = start + 6
+            price = text[start:end].strip()
             
-        cur.execute("INSERT INTO purchases (id, title, type, price, date) VALUES (?, ?, ?, ?, ?)",
-            (msg_id, title, purchase_type, price, date))
+            #Date
+            if ' AM' in text or ' PM' in text:
+                period = ' AM' if ' AM' in text else ' PM'
+                start = text.find(period) - 16
+                end = text.find(period) + 3
+            else:
+                heading = 'Date and time of purchase: '
+                start = text.find(heading) + len(heading)
+                end = start + 11
+            date = text[start:end].strip()
+            
+            if '@' in date:
+                date = text[start-2:end].strip()
+                date = date.replace('@','').replace(' ','',1)
+                
+            purchase_dict = {
+                'Title': title,
+                'Type': purchase_type,
+                'Price': price,
+                'Date': date
+            }
+            
+            print(purchase_dict)
+                
+            cur.execute("INSERT INTO purchases (id, title, type, price, date) VALUES (?, ?, ?, ?, ?)",
+                (msg_id, title, purchase_type, price, date))
+            
+            con.commit()
 
         
       
 if __name__ == '__main__':
-    start()
+    app.run()
